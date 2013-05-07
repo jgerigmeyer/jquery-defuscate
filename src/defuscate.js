1e722dc2a50d94d5a5127f18c5c935b7dcec3cca
Jonny Gerig Meyer <jonny@oddbird.net>
2013-05-07 13:24:41 -0400

    add plugin and unit tests

--- src/defuscate.js
+++ src/defuscate.js
@@ -6,33 +6,68 @@
  * Licensed under the MIT license.
  */
 
-(function($) {
+(function ($) {
 
-  // Collection method.
-  $.fn.awesome = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
-    });
-  };
+    var settings = {};
 
-  // Static method.
-  $.awesome = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
+    var methods = {
+        init: function (options) {
+            settings = $.extend({}, $.fn.defuscate.defaults, options);
+            return this.each(function () {
+                var el, newHref, newHtml, is_link;
+                el = $(this);
+                is_link = false;
+                if (el.is('a[href]')) {
+                    is_link = true;
+                    newHref = methods.defuscateHref(el.attr('href'));
+                    methods.updateHref(el, newHref);
+                }
+                newHtml = methods.defuscateHtml(el.html(), is_link);
+                methods.updateHtml(el, newHtml);
+            });
+        },
+        defuscateHref: function (href) {
+            return href.replace(settings.find, settings.replace);
+        },
+        defuscateHtml: function (html, is_link) {
+            var find = settings.find;
+            var replace = settings.replace;
+            var replacedHTML;
+            if (is_link) {
+                replacedHTML = html.replace(find, replace);
+            } else {
+                replacedHTML = html.replace(find, '<a href="mailto:' + replace + '">' + replace + '</a>');
+            }
+            return replacedHTML;
+        },
+        updateHref: function (el, newHref) {
+            el.attr('href', newHref);
+            return el;
+        },
+        updateHtml: function (el, newHtml) {
+            el.html(newHtml);
+            return el;
+        }
+    };
 
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
-  };
+    // Collection method.
+    $.fn.defuscate = function (method) {
+        // Method calling logic
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.tooltip');
+        }
+    };
 
-  // Custom selector.
-  $.expr[':'].awesome = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
+    // Setup plugin defaults
+    $.fn.defuscate.defaults = {
+        find: /\b([A-Z0-9._%\-]+)\([^)]+\)((?:[A-Z0-9\-]+\.)+[A-Z]{2,6})\b/gi,
+        replace: '$1@$2'
+    };
+
+    settings = $.extend({}, $.fn.defuscate.defaults);
 
 }(jQuery));
--- test/defuscate_test.js
+++ test/defuscate_test.js
@@ -1,62 +1,96 @@
-(function($) {
-  /*
+(function ($) {
+    /*
     ======== A Handy Little QUnit Reference ========
     http://api.qunitjs.com/
 
     Test methods:
-      module(name, {[setup][ ,teardown]})
-      test(name, callback)
-      expect(numberOfAssertions)
-      stop(increment)
-      start(decrement)
+        module(name, {[setup][ ,teardown]})
+        test(name, callback)
+        expect(numberOfAssertions)
+        stop(increment)
+        start(decrement)
     Test assertions:
-      ok(value, [message])
-      equal(actual, expected, [message])
-      notEqual(actual, expected, [message])
-      deepEqual(actual, expected, [message])
-      notDeepEqual(actual, expected, [message])
-      strictEqual(actual, expected, [message])
-      notStrictEqual(actual, expected, [message])
-      throws(block, [expected], [message])
-  */
+        ok(value, [message])
+        equal(actual, expected, [message])
+        notEqual(actual, expected, [message])
+        deepEqual(actual, expected, [message])
+        notDeepEqual(actual, expected, [message])
+        strictEqual(actual, expected, [message])
+        notStrictEqual(actual, expected, [message])
+        throws(block, [expected], [message])
+    */
 
-  module('jQuery#awesome', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
+    module('jQuery#defuscate', {
+        // This will run before each test in this module.
+        setup: function () {
+            this.elems = $('#qunit-fixture .tests').children();
+            this.results = $('#qunit-fixture .results').children();
+        }
+    });
 
-  test('is chainable', function() {
-    expect(1);
-    // Not a bad test to run on collection methods.
-    strictEqual(this.elems.awesome(), this.elems, 'should be chainable');
-  });
+    test('is chainable', 1, function() {
+        strictEqual(this.elems.defuscate(), this.elems, 'should be chainable');
+    });
 
-  test('is awesome', function() {
-    expect(1);
-    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
-  });
+    test('defuscateHref', 3, function () {
+        strictEqual(this.elems.eq(0).defuscate('defuscateHref', this.elems.eq(0).attr('href')), this.results.eq(0).attr('href'));
+        strictEqual(this.elems.eq(1).defuscate('defuscateHref', this.elems.eq(1).attr('href')), this.results.eq(1).attr('href'));
+        strictEqual(this.elems.eq(2).defuscate('defuscateHref', this.elems.eq(2).attr('href')), this.results.eq(2).attr('href'));
+    });
 
-  module('jQuery.awesome');
+    test('defuscateHtml', 6, function () {
+        strictEqual(this.elems.eq(0).defuscate('defuscateHtml', this.elems.eq(0).html(), true), this.results.eq(0).html());
+        strictEqual(this.elems.eq(0).defuscate('defuscateHtml', this.elems.eq(1).html(), true), this.results.eq(1).html());
+        strictEqual(this.elems.eq(0).defuscate('defuscateHtml', this.elems.eq(2).html(), true), this.results.eq(2).html());
+        strictEqual(this.elems.eq(0).defuscate('defuscateHtml', this.elems.eq(3).html(), false), this.results.eq(3).html());
+        strictEqual(this.elems.eq(0).defuscate('defuscateHtml', this.elems.eq(4).html(), false), this.results.eq(4).html());
+        strictEqual(this.elems.eq(0).defuscate('defuscateHtml', this.elems.eq(5).html(), false), this.results.eq(5).html());
+    });
 
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.awesome(), 'awesome.', 'should be awesome');
-    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-  });
+    test('updateHref', 3, function () {
+        strictEqual(
+            this.elems.eq(0).defuscate('updateHref', this.elems.eq(0), this.results.eq(0).attr('href')).attr('href'),
+            this.results.eq(0).attr('href')
+        );
+        strictEqual(
+            this.elems.eq(1).defuscate('updateHref', this.elems.eq(1), this.results.eq(1).attr('href')).attr('href'),
+            this.results.eq(1).attr('href')
+        );
+        strictEqual(
+            this.elems.eq(2).defuscate('updateHref', this.elems.eq(2), this.results.eq(2).attr('href')).attr('href'),
+            this.results.eq(2).attr('href')
+        );
+    });
 
-  module(':awesome selector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
+    test('updateHtml', 6, function () {
+        strictEqual(
+            this.elems.eq(0).defuscate('updateHtml', this.elems.eq(0), this.results.eq(0).html()).html(),
+            this.results.eq(0).html()
+        );
+        strictEqual(
+            this.elems.eq(1).defuscate('updateHtml', this.elems.eq(1), this.results.eq(1).html()).html(),
+            this.results.eq(1).html()
+        );
+        strictEqual(
+            this.elems.eq(2).defuscate('updateHtml', this.elems.eq(2), this.results.eq(2).html()).html(),
+            this.results.eq(2).html()
+        );
+        strictEqual(
+            this.elems.eq(3).defuscate('updateHtml', this.elems.eq(3), this.results.eq(3).html()).html(),
+            this.results.eq(3).html()
+        );
+        strictEqual(
+            this.elems.eq(4).defuscate('updateHtml', this.elems.eq(4), this.results.eq(4).html()).html(),
+            this.results.eq(4).html()
+        );
+        strictEqual(
+            this.elems.eq(5).defuscate('updateHtml', this.elems.eq(5), this.results.eq(5).html()).html(),
+            this.results.eq(5).html()
+        );
+    });
 
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
-  });
+    test('integration', 1, function () {
+        strictEqual(this.elems.defuscate().html(), this.results.html());
+    });
 
 }(jQuery));
